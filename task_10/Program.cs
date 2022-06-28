6a4df4f64967177c0863c215818ed398365e528c
Sergey_Mikhaylenko <Slad95@mail.ru>
2022-06-28 17:32:57 +0400

    task10

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number);
+
+string result = Convert.ToString(number);
+
+Console.WriteLine(result[1]);
